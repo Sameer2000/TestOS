39331a2abe0bc590a8c566e4ad59a1709231cdf7
Sameer2000 <smrgrover2@gmail.com>
2016-02-01 00:33:31 +0530

    Upload OS

---
+++ TestOS/src/main/java/com/app/testOS/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.app.testOS;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestOsApplication.class);
+	}
+
+}
---
+++ TestOS/src/main/java/com/app/testOS/TestOsApplication.java
@@ -0,0 +1,12 @@
+package com.app.testOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOsApplication.class, args);
+	}
+}
